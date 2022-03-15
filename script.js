bf7f43e9bcf71cff1221bbad2709af86ad358c07
Glitch (shocking-plaid-ghoul) <none>
2022-03-15 02:20:32 +0000

    🎁🐵 Checkpoint ./script.js:43056240/961 ./index.html:43056240/121

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 //Global Variables
 var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
 var progress = 0;
-var gamePlaying
+var gamePlaying = false;
+
+function startGame(){
+  //initialize game variable
+  progress = 0;
+  gamePlaying = true;
+  
+  //swap the start and stop buttons 
+document.getElementById("startBtn").classList.add("hidden");
+document.getElementById("stopBtn").classList.remove("hidden");
+
+}
+
+function stopGame(){
+  //no need to update progress
+  gamePlaying = false;
+  
+  //switch start and stop buttons
+  document.getElementById("stopBtn").classList.add("hidden");
+  document.getElementById("startBtn").classList.remove("hidden");
+  
+}
+ 
+
