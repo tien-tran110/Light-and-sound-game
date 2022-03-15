45010e406ce30051e485f6aa5d15b024a5e37994
Glitch (shocking-plaid-ghoul) <none>
2022-03-15 02:44:32 +0000

    😄💴 Checkpoint ./script.js:43056240/1042

--- script.js
+++ script.js
@@ -23,6 +23,17 @@ function stopGame(){
   document.getElementById("stopBtn").classList.add("hidden");
   document.getElementById("startBtn").classList.remove("hidden");
 }
+
+//Initilization
+//Init Sound Synthesizer
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var context = new AudioContext();
+var o = context.createOscillator();
+var g = context.createGain();
+g.connect(context.destination);;
+g.gain.setValueAtTime(0, context.currentTime);
+o.connect(g);
+o.start(0);
  
 //Sound Synthesis Function
 const freqMap = {
@@ -33,6 +44,24 @@ const freqMap = {
 }
 function playTone(btn, len){
   o.frequency.value = freqMap[btn];
-  
+  g.gain.setTargetAtTime(volume,context.currentTime +0.05, 0.025);
+  context.resume();
+  tonePlaying = true;
+  setTimeOut(function(){
+             stopTone();
+             }, len)
+}
+function startTone(btn){
+  if(!tonePlaying){
+    context.resume();
+    o.frequency.value = freqMap[btn];
+    g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025);
+    context.resume();
+    tonePlaying = true;
+  }
+}
+function stopTone(){
+  g.gain.setTargetAtTime(0, context.currentTime + 0.05, 0.025);
+  tonePlaying = false;
 }
 
