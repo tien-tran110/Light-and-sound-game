9593e45e98a99d1ac9fc5d67532f965d61e5d92e
Glitch (shocking-plaid-ghoul) <none>
2022-03-15 03:43:31 +0000

    🗺👔 Checkpoint ./index.html:43056240/455 ./script.js:43056240/5985

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //global constant
 const clueHoldTime = 1000; // how long to hold each clue light/sound
-const cluePauseTime= 333;//how long to pause in between clues
-const nextClueWaitTime = 1000;//how long to wait before playing sequence
+const cluePauseTime = 333; //how long to pause in between clues
+const nextClueWaitTime = 1000; //how long to wait before playing sequence
 
 //Global Variables
 var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
@@ -11,21 +11,21 @@ var tonePlaying = false;
 var volume = 0.5; //betwwen 0.0 and 1.0
 var guessCounter = 0;
 
-function startGame(){
+function startGame() {
   //initialize game variable
   progress = 0;
   gamePlaying = true;
-  
-  //swap the start and stop buttons 
+
+  //swap the start and stop buttons
   document.getElementById("startBtn").classList.add("hidden");
   document.getElementById("stopBtn").classList.remove("hidden");
   playClueSequence();
 }
 
-function stopGame(){
+function stopGame() {
   //no need to update progress
   gamePlaying = false;
-  
+
   //switch start and stop buttons
   document.getElementById("stopBtn").classList.add("hidden");
   document.getElementById("startBtn").classList.remove("hidden");
@@ -37,29 +37,29 @@ var AudioContext = window.AudioContext || window.webkitAudioContext;
 var context = new AudioContext();
 var o = context.createOscillator();
 var g = context.createGain();
-g.connect(context.destination);;
+g.connect(context.destination);
 g.gain.setValueAtTime(0, context.currentTime);
 o.connect(g);
 o.start(0);
- 
+
 //Sound Synthesis Function
 const freqMap = {
   1: 261.6,
   2: 329.6,
   3: 329,
-  4: 466.2
-}
-function playTone(btn, len){
+  4: 466.2,
+};
+function playTone(btn, len) {
   o.frequency.value = freqMap[btn];
-  g.gain.setTargetAtTime(volume,context.currentTime +0.05, 0.025);
+  g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025);
   context.resume();
   tonePlaying = true;
-  setTimeout(function(){
-             stopTone()
-             }, len);
+  setTimeout(function () {
+    stopTone();
+  }, len);
 }
-function startTone(btn){
-  if(!tonePlaying){
+function startTone(btn) {
+  if (!tonePlaying) {
     context.resume();
     o.frequency.value = freqMap[btn];
     g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025);
@@ -67,31 +67,30 @@ function startTone(btn){
     tonePlaying = true;
   }
 }
-function stopTone(){
+function stopTone() {
   g.gain.setTargetAtTime(0, context.currentTime + 0.05, 0.025);
   tonePlaying = false;
 }
 
-function lightButton(btn){
+function lightButton(btn) {
   document.getElementById("button" + btn).classList.add("lit");
 }
-function clearButton(btn){
-  document.getElementById("button"+btn).classList.remove("lit");
+function clearButton(btn) {
+  document.getElementById("button" + btn).classList.remove("lit");
 }
 
-function playingSingleClue(btn){
-  if(gamePlaying){
+function playingSingleClue(btn) {
+  if (gamePlaying) {
     lightButton(btn);
     playTone(btn, clueHoldTime);
     setTimeout(clearButton, clueHoldTime, btn);
   }
 }
 
-function playClueSequence(){
+function playClueSequence() {
   guessCounter = 0;
-  context.resume();
   let delay = nextClueWaitTime; //set delay to initial wait time
-  for(let i = 0;i<=progress;i++){
+  for (let i = 0; i <= progress; i++) {
     //for each clue that is revealed so far
     console.log("Play Single Clue: " + pattern[i] + " in " + delay + "ms");
     setTimeout(playingSingleClue, delay, pattern[i]); //set a timeout to play that clue
@@ -100,22 +99,21 @@ function playClueSequence(){
   }
 }
 
-function loseGame(){
+function loseGame() {
   stopGame();
   alert("Game Over. Try again!");
 }
 
-function winGame(){
-  stopGame()
+function winGame() {
+  stopGame();
   alert("You are the champion!");
 }
 
-funtion guess(btn){
+function guess(btn) {
   console.log("User guessed: " + btn);
-  if(!gamePlaying){
+  if (!gamePlaying) {
     return;
   }
-  
+
   //game logic
-  
 }
