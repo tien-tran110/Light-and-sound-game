b2dc1d924ec579e4a89ac5225dcc08a7486a79e4
Glitch (shocking-plaid-ghoul) <none>
2022-03-15 02:32:32 +0000

    🚈💖 Checkpoint ./script.js:43056240/271

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
 var progress = 0;
 var gamePlaying = false;
+var tonePlaying = false;
+var volume = 0.5; //betwwen 0.0 and 1.0
 
 function startGame(){
   //initialize game variable
@@ -11,7 +13,6 @@ function startGame(){
   //swap the start and stop buttons 
 document.getElementById("startBtn").classList.add("hidden");
 document.getElementById("stopBtn").classList.remove("hidden");
-
 }
 
 function stopGame(){
@@ -21,7 +22,17 @@ function stopGame(){
   //switch start and stop buttons
   document.getElementById("stopBtn").classList.add("hidden");
   document.getElementById("startBtn").classList.remove("hidden");
-  
 }
  
+//Sound Synthesis Function
+const freqMap = {
+  1: 261.6,
+  2: 329.6,
+  3: 329,
+  4: 466.2
+}
+function playTone(btn, len){
+  o.frequency.value = freqMap[btn];
+  
+}
 
